8c73abc353b526e70242a3cb9ea1e489787884c9
Baig Zuber <108341407+zuber710@users.noreply.github.com>
2024-01-03 14:14:24 +0530

    check_service

--- check_service.sh
+++ check_service.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+read -p "which service you want to check:- " SERVICE
+STATUS=service $SERVICE status | grep Active | awk '{print $2}'
+
+if [[ $STATUS == active ]]
+then
+    echo -e "Service is Active"
+else
+    echo "Service is not installed"
+    
+    read -p "Do you want to start this service (yes/no) :- " CHOICE
+
+    if [[ $CHOICE == yes ]]
+    then
+    apt install $SERVICE -y
+        echo -e "Service installed successfully...!!"
+    service $SERVICE restart
+        echo -e "$SERVICE service is started..!"
+    else
+        echo -e "Thanks...!!"
+    fi
+fi

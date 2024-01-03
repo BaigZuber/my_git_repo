16f07495c8abc8f9c3882e237310ad9b9eea4b9c
Baig Zuber <108341407+zuber710@users.noreply.github.com>
2024-01-03 14:06:57 +0530

    send_disk_alert.sh

--- send_disk_alert.sh
+++ send_disk_alert.sh
@@ -1,1 +1,11 @@
-hiii
+#!/bin/bash
+
+ALERT=75
+USAGE=`df -h | grep rootfs | tr -d % | awk '{ print $5 }'` 
+
+if [[ $USAGE -gt $ALERT ]]
+then
+    echo -e "disk usage is above threshold.."
+else
+    echo -e "disk usage is under threshold and fine..!"
+fi
